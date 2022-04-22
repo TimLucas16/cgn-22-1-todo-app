07405b8feb15dbd7acd4afd9167884854304c27a
maxschemann <schemann@phonetix.net>
2022-04-22 14:18:45 +0200

    Diasplay filtering

--- frontend/src/component/Display.tsx
+++ frontend/src/component/Display.tsx
@@ -1,7 +1,28 @@
 import CardList from "./CardList";
+import useTodos from "../hook/useTodos";
+import {Todo} from "../model/Todo";
 
 export default function Display() {
+
+    const todos: Todo [] = useTodos
+
+    const todosOpen = todos.filter(
+        t => t.status === "open"
+    )
+
+    const todosPending = todos.filter(
+        t => t.status === "pending"
+    )
+
+    const todosClosed = todos.filter(
+        t => t.status === "closed"
+    )
+
     return (
-         <CardList/>
+        <div>
+        <CardList todos={todosOpen}/>
+        <CardList todos={todosPending}/>
+        <CardList todos={todosClosed}/>
+        </div>
     )
 }
